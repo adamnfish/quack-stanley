cd4f1d892ecb147e337cf344f6983224c1824191
adamnfish <adamnfish@gmail.com>
2018-02-05 23:00:21 +0000

    Tidy up dev server implementation

--- dev-server/src/main/scala/devserver/ApiService.scala
+++ dev-server/src/main/scala/devserver/ApiService.scala
@@ -1,18 +1,11 @@
 package devserver
 
-import java.io.ByteArrayInputStream
-
-import com.adamnfish.quackstanley.{Config, Main}
-import com.adamnfish.quackstanley.attempt.Attempt
-import com.adamnfish.quackstanley.attempt.LambdaIntegration.{failureToJson, parseBody, parseLambdaRequestBody}
-import com.adamnfish.quackstanley.models.{ApiOperation, Serialization}
+import com.adamnfish.quackstanley.attempt.LambdaIntegration.failureToJson
 import com.adamnfish.quackstanley.models.Serialization._
-import fs2.Task
+import com.adamnfish.quackstanley.models.{ApiOperation, Serialization}
+import com.adamnfish.quackstanley.{Config, Main}
 import io.circe.Json
 import io.circe.syntax._
-import org.http4s.dsl.impl.EntityResponseGenerator
-import org.http4s.dsl.{->, /, POST, Root}
-import org.http4s.{HttpService, Status}
 
 import scala.concurrent.Future
 
@@ -26,29 +19,17 @@ object ApiService {
 
   def devQuackStanley(body: Json)(): Future[(Int, String)] = {
     (for {
-      apiOp <- extract(body)
-      statusAndResponse <- executeOperation(apiOp)
-    } yield statusAndResponse).asFuture.map(_.left.map(fa => (fa.statusCode, fa.logString)).merge)
-  }
-
-  def extract(body: Json): Attempt[ApiOperation] = {
-    for {
-      a <- Serialization.extractJson[ApiOperation](body)
-    } yield a
-  }
-
-  def executeOperation(apiOperation: ApiOperation): Attempt[(Int, String)] = {
-    Attempt.Async.Right {
-      quackStanley.dispatch(apiOperation, fakeContext, fakeConfig).asFuture.map {
-        case Right(out) =>
-          (200, out.asJson.noSpaces)
-        case Left(failures) =>
-          fakeContext.getLogger.log(s"Failure: ${failures.logString}")
-          val body = Json.obj(
-            "errors" -> Json.fromValues(failures.failures.map(failureToJson))
-          )
-          (failures.statusCode, body.noSpaces)
-      }
+      apiOp <- Serialization.extractJson[ApiOperation](body)
+      statusAndResponse <- quackStanley.dispatch(apiOp, fakeContext, fakeConfig)
+    } yield statusAndResponse).asFuture.map {
+      case Right(out) =>
+        (200, out.asJson.noSpaces)
+      case Left(failures) =>
+        fakeContext.getLogger.log(s"Failure: ${failures.logString}")
+        val body = Json.obj(
+          "errors" -> Json.fromValues(failures.failures.map(failureToJson))
+        )
+        (failures.statusCode, body.noSpaces)
     }
   }
 }
--- dev-server/src/main/scala/devserver/DevServer.scala
+++ dev-server/src/main/scala/devserver/DevServer.scala
@@ -5,22 +5,21 @@ import devserver.ApiService.devQuackStanley
 import fs2.{Strategy, Task}
 import io.circe.Json
 import org.http4s.circe._
+import org.http4s.client.blaze._
 import org.http4s.dsl.impl.EntityResponseGenerator
-import org.http4s.{Header, HttpService, Status}
 import org.http4s.dsl.{Root, _}
 import org.http4s.server.blaze._
 import org.http4s.util.StreamApp
-import org.http4s.client.blaze._
-import org.http4s.headers.`Content-Type`
-import org.http4s.MediaType.`text/html`
+import org.http4s.{HttpService, Status}
 
 
 object DevServer extends StreamApp with LazyLogging {
   implicit val ec = scala.concurrent.ExecutionContext.global
   implicit val strategy = Strategy.sequential
 
-  val httpClient = PooledHttp1Client()
+  class ArbitraryStatus(val status: Status) extends AnyVal with EntityResponseGenerator
 
+  val httpClient = PooledHttp1Client()
   val elmFrontend = HttpService {
     case request @ GET -> _ =>
       logger.info(s"request: ${request.pathInfo}")
@@ -32,8 +31,6 @@ object DevServer extends StreamApp with LazyLogging {
         } yield result
       }
   }
-
-  class ArbitraryStatus(val status: Status) extends AnyVal with EntityResponseGenerator
 
   val lambdaApi = HttpService {
     case request @ POST -> Root =>
