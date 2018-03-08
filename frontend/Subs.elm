55d5b8bb069562ce1889535f7878bd0d8850f12a
Adam Fisher <adam.fisher@guardian.co.uk>
2018-03-08 18:20:48 +0000

    Only ping when required

--- frontend/Subs.elm
+++ frontend/Subs.elm
@@ -1,10 +1,16 @@
 module Subs exposing (subscriptions)
 
-import Model exposing (Model)
+import Model exposing (Model, Lifecycle (Waiting, Spectating))
 import Msg exposing (Msg (PingEvent))
 import Time
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every ( 5 * Time.second ) PingEvent
+    case model.lifecycle of
+        Waiting ->
+            Time.every ( 5 * Time.second ) PingEvent
+        Spectating _ ->
+            Time.every ( 15 * Time.second ) PingEvent
+        _ ->
+            Sub.none
