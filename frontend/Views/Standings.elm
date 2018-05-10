e558a006f082a4b004bc620392930482fa326632
adamnfish <adamnfish@gmail.com>
2018-05-10 19:00:41 +0100

    Add standings page (not linked up yet)

---
+++ frontend/Views/Standings.elm
@@ -0,0 +1,48 @@
+module Views.Standings exposing (standings)
+
+import Html exposing (Html, div, text, button, ul, li, span)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onSubmit, onInput)
+import Model exposing (Model, PlayerSummary, Lifecycle (..))
+import Msg exposing (Msg)
+import Views.Utils exposing (container, row, col, card, icon, plural)
+
+
+standings : Model -> Html Msg
+standings model =
+    let
+        points = Maybe.withDefault [] ( Maybe.map .points model.state )
+    in
+        container "standings"
+            [ row
+                [ col "s12"
+                    [ ul
+                        [ class "collection z-depth-1" ]
+                        (
+                            [ li
+                                [ class "collection-item" ]
+                                [ text "You"
+                                , span
+                                    [ class "badge"
+                                    , attribute "data-badge-caption" ( plural "point" ( List.length points ) )
+                                    ]
+                                    [ text ( toString ( List.length points ) ) ]
+                                ]
+                            ] ++ List.map playerListEntry model.opponents
+                        )
+                    ]
+
+                ]
+            ]
+
+playerListEntry : PlayerSummary -> Html Msg
+playerListEntry playerSummary =
+    li
+        [ class "collection-item" ]
+        [ span
+            [ class "badge"
+            , attribute "data-badge-caption" ( plural "point" ( List.length playerSummary.points ) )
+            ]
+            [ text ( toString ( List.length playerSummary.points ) ) ]
+        , text playerSummary.screenName
+        ]
