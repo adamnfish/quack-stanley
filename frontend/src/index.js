34a48d0f14afd49957280ab9848832d6424b400d
adamnfish <adamnfish@gmail.com>
2018-05-12 00:30:23 +0100

    Add styles / images / fonts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
-import './main.css';
+import './css/materialize.css';
+import './css/main.css';
 import { Main } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
 
 Main.embed(document.getElementById('root'));
-
-registerServiceWorker();
