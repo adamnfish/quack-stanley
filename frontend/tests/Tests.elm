f4c0cda8eb8f7749019e0506f2787c818aeb4941
adamnfish <adamnfish@gmail.com>
2020-11-14 16:30:36 +0000

    Restore theme colour, remove defunct files
    The old (black) theme colour looked better. Also removed some
    generated files that are not needed.

--- frontend/tests/Tests.elm
+++
@@ -1,7 +0,0 @@
-module Tests exposing (..)
-
-import Test exposing (..)
-import Expect
-
-
--- Check out http://package.elm-lang.org/packages/elm-community/elm-test/latest to learn more about testing in Elm!
